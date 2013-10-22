eb669e2659ef3d4272690d55de1d3b481a34bde5
yifange <irfan.1989@gmail.com>
2013-10-22 14:45:25 -0400

    static view now works

--- tileflow.py
+++ tileflow.py
@@ -3,6 +3,7 @@ from PySide import QtCore, QtGui, QtOpenGL
 
 try:
     from OpenGL.GL import *
+    from OpenGL import GLU
 except ImportError:
     app = QtGui.QApplication(sys.argv)
     QtGui.QMessageBox.critical(None, "OpenGL textures",
@@ -13,14 +14,18 @@ except ImportError:
 
 class TileflowWidget(QtOpenGL.QGLWidget):
 
-    coords = (
-        ( ( +1, -1, -1 ), ( -1, -1, -1 ), ( -1, +1, -1 ), ( +1, +1, -1 ) ),
-        ( ( +1, +1, -1 ), ( -1, +1, -1 ), ( -1, +1, +1 ), ( +1, +1, +1 ) ),
-        ( ( +1, -1, +1 ), ( +1, -1, -1 ), ( +1, +1, -1 ), ( +1, +1, +1 ) ),
-        ( ( -1, -1, -1 ), ( -1, -1, +1 ), ( -1, +1, +1 ), ( -1, +1, -1 ) ),
-        ( ( +1, -1, +1 ), ( -1, -1, +1 ), ( -1, -1, -1 ), ( +1, -1, -1 ) ),
-        ( ( -1, -1, +1 ), ( +1, -1, +1 ), ( +1, +1, +1 ), ( -1, +1, +1 ) )
-    )
+    GVertices = [
+        -1.0, -1.0, 0.0,
+        1.0, -1.0, 0.0,
+        -1.0, 1.0, 0.0,
+        1.0, 1.0, 0.0
+    ]
+    GTextures = [
+        0.0, 1.0,
+        1.0, 1.0,
+        0.0, 0.0,
+        1.0, 0.0
+    ]
     SCALE = 0.7
     SPREAD_IMAGE = 0.14
     FLANK_SPREAD = 0.4
@@ -32,56 +37,60 @@ class TileflowWidget(QtOpenGL.QGLWidget):
         QtOpenGL.QGLWidget.__init__(self, parent)
 
         # self.clearColor = QtCore.Qt.black
-        self.xRot = 0
-        self.yRot = 0
-        self.zRot = 0
         self.mWidth = 0
         self.clearColor = QtGui.QColor()
         self.lastPos = QtCore.QPoint()
-        self.obj = None
+        self.tiles = []
 
-    # def freeGLResources(self):
-    #     TileflowWidget.refCount -= 1
-    #     if TileflowWidget.refCount == 0:
-    #         self.makeCurrent()
-    #         glDeleteLists(self.__class__.obj, 1)
 
     def minimumSizeHint(self):
-        return QtCore.QSize(50, 50)
+        return QtCore.QSize(533, 270)
 
     def sizeHint(self):
-        return QtCore.QSize(200, 200)
+        return QtCore.QSize(533, 270)
 
-    def rotateBy(self, xAngle, yAngle, zAngle):
-        self.xRot = (self.xRot + xAngle) % 5760
-        self.yRot = (self.yRot + yAngle) % 5760
-        self.zRot = (self.zRot + zAngle) % 5760
-        self.updateGL()
 
     def setClearColor(self, color):
         self.clearColor = color
         self.updateGL()
 
     def initializeGL(self):
-        if not self.obj:
-            self.tiles = []
-            for i in range(6):
-                self.tiles.append(Tile(self.bindTexture(QtGui.QPixmap("images/side%d.png" % (i + 1)))))
-            self.obj = self.makeObject()
-
-        glEnable(GL_DEPTH_TEST)
-        glEnable(GL_CULL_FACE)
-        glEnable(GL_TEXTURE_2D)
+        for i in range(6):
+            self.tiles.append(Tile(self.bindTexture(QtGui.QPixmap("images/side%d.png" % (i + 1)))))
+        self.verticesBuffer = TileflowWidget.GVertices
+        self.texturesBuffer = TileflowWidget.GTextures
 
     def paintGL(self):
-        self.qglClearColor(self.clearColor)
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
-        glTranslated(0.0, 0.0, -10.0)
-        glRotated(self.xRot / 16.0, 1.0, 0.0, 0.0)
-        glRotated(self.yRot / 16.0, 0.0, 1.0, 0.0)
-        glRotated(self.zRot / 16.0, 0.0, 0.0, 1.0)
-        glCallList(self.obj)
+        GLU.gluLookAt(0, 0, 2, 0, 0, 0, 0, 1, 0)
+        glDisable(GL_DEPTH_TEST)
+
+        glClearColor(0, 0, 0, 0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+
+
+
+
+        glPushMatrix()
+        glEnable(GL_TEXTURE_2D)
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        # offset = self.offset
+        offset = 3
+        mid = 3
+        # startPos = mid - VISIBLE_TILES
+        # if startPos < 0:
+        #     startPos = 0
+
+        for i in range(0, mid):
+            self.drawTile(i, i - offset, self.tiles[i])
+        for i in range(5, mid - 1, -1):
+            self.drawTile(i, i - offset, self.tiles[i])
+
+        glPopMatrix()
+
 
     def resizeGL(self, width, height):
         mWidth = width
@@ -89,10 +98,18 @@ class TileflowWidget(QtOpenGL.QGLWidget):
         imageh = height * 0.45 / TileflowWidget.SCALE / 2.0
 
         glViewport(0, 0, width, height)
-        ratio = width / height
+        ratio = float(width) / height
+        print ratio
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
         glOrtho(-ratio * TileflowWidget.SCALE, ratio * TileflowWidget.SCALE, -1 * TileflowWidget.SCALE, 1 * TileflowWidget.SCALE, 1, 3)
+        self.verticesBuffer = [
+            -ratio * TileflowWidget.SCALE, -TileflowWidget.SCALE, 0,
+            ratio * TileflowWidget.SCALE, -TileflowWidget.SCALE, 0,
+            -ratio * TileflowWidget.SCALE, TileflowWidget.SCALE, 0,
+            ratio * TileflowWidget.SCALE, TileflowWidget.SCALE, 0
+        ]
+        # print self.verticesBuffer
         # glMatrixMode(GL_MODELVIEW)
 
     def mousePressEvent(self, event):
@@ -102,33 +119,11 @@ class TileflowWidget(QtOpenGL.QGLWidget):
         dx = event.x() - self.lastPos.x()
         dy = event.y() - self.lastPos.y()
 
-        if event.buttons() & QtCore.Qt.LeftButton:
-            self.rotateBy(8 * dy, 8 * dx, 0)
-        elif event.buttons() & QtCore.Qt.RightButton:
-            self.rotateBy(8 * dy, 0, 8 * dx)
 
         self.lastPos = QtCore.QPoint(event.pos())
 
     def mouseReleaseEvent(self, event):
         self.clicked.emit()
-
-    def makeObject(self):
-        dlist = glGenLists(1)
-        glNewList(dlist, GL_COMPILE)
-        offset = 3
-        mid = 3
-        # startPos = mid - VISIBLE_TILES
-        # if startPos < 0:
-        #     startPos = 0
-
-        for i in range(0, mid):
-            self.drawTile(i, i - offset, self.tiles[i])
-        for i in range(5, mid, -1):
-            self.drawTile(i, i - offset, self.tiles[i])
-        # for i in range(6):
-        #     self.drawTile(i, 0, self.tiles[i])
-        glEndList()
-        return dlist
 
     def drawTile(self, position, off, tile):
         matrix = [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0]
@@ -144,19 +139,36 @@ class TileflowWidget(QtOpenGL.QGLWidget):
         sc = 0.38 * matrix[0]
         trans += f * 1
 
-        print matrix
         glPushMatrix()
         glBindTexture(GL_TEXTURE_2D, tile.texture)
-
         glTranslatef(trans, 0, 0)
         glScalef(sc, sc, 1.0)
         glMultMatrixf(matrix)
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
 
-        glTranslatef(0, -2, 0)
+        glBegin(GL_QUADS)
+        glTexCoord2d(1, 0)
+        glVertex3d(1, -1, 0)
+        glTexCoord2d(0, 0)
+        glVertex3d(-1, -1, 0)
+        glTexCoord2d(0, 1)
+        glVertex3d(-1, 1, 0)
+        glTexCoord2d(1, 1)
+        glVertex3d(1, 1, 0)
+        glEnd()
+        # glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
+        glTranslatef(0, -2.0, 0)
         glScalef(1, -1, 1)
         glColor4f(1, 1, 1, 0.5)
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
+        glBegin(GL_QUADS)
+        glTexCoord2d(1, 0)
+        glVertex3d(1, -1, 0)
+        glTexCoord2d(0, 0)
+        glVertex3d(-1, -1, 0)
+        glTexCoord2d(0, 1)
+        glVertex3d(-1, 1, 0)
+        glTexCoord2d(1, 1)
+        glVertex3d(1, 1, 0)
+        glEnd()
         glColor4f(1, 1, 1, 1)
 
         glPopMatrix()
